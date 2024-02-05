6b90fe0ff1e83b1b1ad31586a1616418678663de
salozzo86 <antonio.malafronte86@gmail.com>
2024-02-05 22:46:53 +0100

    Updated comment with time and space complexity

--- Arrays/EvenNumberDigits.java
+++ Arrays/EvenNumberDigits.java
@@ -14,3 +14,14 @@ class EvenNumberDigits {
         return count;
     }
 }
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+/*
+Given an array nums of integers, return how many of them contain an even number of digits.
+
+Example 1:
+Input: nums = [12,345,2,6,7896]
+Output: 2
+ */
