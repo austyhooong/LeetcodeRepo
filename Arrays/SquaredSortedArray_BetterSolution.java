542be3244a73d5ab1383198d3890545551d19705
salozzo86 <antonio.malafronte86@gmail.com>
2024-02-17 16:35:33 +0100

    Added comments with O notation

--- Arrays/SquaredSortedArray_BetterSolution.java
+++ Arrays/SquaredSortedArray_BetterSolution.java
@@ -44,3 +44,17 @@ class SquaredSortedArray_BetterSolution {
         return finalArray;
     }
 }
+
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
+
+/*
+https://leetcode.com/problems/squares-of-a-sorted-array/description/
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Example 1:
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+ */
