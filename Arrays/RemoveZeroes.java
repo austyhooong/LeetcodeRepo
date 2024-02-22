1ec4446324cdf72fe433ed588c776e8b2432926c
salozzo86 <antonio.malafronte86@gmail.com>
2024-02-22 14:32:42 +0100

    Solution for Remove Zeroes

---
+++ Arrays/RemoveZeroes.java
@@ -0,0 +1,29 @@
+class RemoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int l = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] != 0) {
+                int temp = nums[r];
+                nums[r] = nums[l];
+                nums[l] = temp;
+                l++;
+            }
+        }
+    }
+}
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+https://leetcode.com/problems/move-zeroes/
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+ */
